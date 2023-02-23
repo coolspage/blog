36f58ec15d7dfd8af099f9f7332c79565770e48c
AngelaCool <cool@gayhub.eu.org>
2023-02-23 18:12:15 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
+<script setup>
+import {ref} from 'vue'
+import login from "./components/login.vue";
+import newB from "./components/newB.vue";
+import edit from "./components/edit.vue";
 
-<script>
-export default {
-  name: "App",
-};
+const pageRef = ref({current: newB})
+const activeIndex = ref('1')
+const activeIndex2 = ref('1')
+const handleSelect = (key, keyPath) => {
+  switch (key) {
+    case "1":
+      pageRef.value.current = newB;
+      break
+    case "2":
+      pageRef.value.current = edit;
+      break
+    case "3":
+      pageRef.value.current = login;
+      break
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
-}
+<template>
+  <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+  >
+    <el-menu-item index="1">最新</el-menu-item>
+    <el-menu-item index="2">发送推文</el-menu-item>
+    <el-menu-item index="3">注册or登录</el-menu-item>
+  </el-menu>
+  <component :is="pageRef.current">
+
+
+  </component>
+</template>
+
+<style scoped>
+
 </style>
---
+++ src/components/newB.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1 class="title">浏览推文</h1>
+    <el-card v-for="(post,i) in posts" shadow="hover" class="iteml">
+      <template #header>
+        {{ post.title }}
+      </template>
+      {{ post.body }}
+    </el-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    axios.get('http://20.210.90.22:23000/posts')
+        .then(response => {
+          this.posts = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        })
+  }
+}
+</script>
+
+<style>
+.title{
+  text-align: center;
+  line-height: 60px;
+  margin-top: 20px;
+}
+.iteml {
+  margin: 20px;
+}
+</style>
